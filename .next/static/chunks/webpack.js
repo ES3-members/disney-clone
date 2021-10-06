dfaec1ff69979524ef2854e8843c292f32cb77b1
Naoto-Yoshimura <yoshimura@stu.kobe-u.ac.jp>
2021-10-06 19:07:01 +0900

    [add] integrated auth0 and disny clone to some extent

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8c60d12b6f257fcb133b"; }
+/******/ 		__webpack_require__.h = function() { return "7031eb1a0fc37ce2f845"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,146 +1,37 @@
-import {gql, GraphQLClient} from 'graphql-request'
-import Link from 'next/Link'
-import Image from 'next/Image'
-import Section from "../components/Section"
-import NavBar from "../components/NavBar"
-import disneyLogo from '../public/disney-button.png'
-import marvelLogo from '../public/marvel-button.png'
-import natgeoLogo from '../public/natgeo-button.png'
-import starwarsLogo from '../public/star-wars-button.png'
-import pixarLogo from '../public/pixar.png'
+import Layout from '../components/Layout'
+import { useFetchUser } from '../lib/user'
 
-export const getStaticProps = async () => {
-    const url = process.env.GRAPH_CMS_ENDPOINT
-    const graphQLClient = new GraphQLClient(url, {
-        headers: {
-            "Authorization": `Bearer ${process.env.GRAPH_CMS_TOKEN}`
-        }
-    })
+function Home() {
+  const { user, loading } = useFetchUser()
 
-    const videosQuery = gql`
-    query {
-      videos {
-        createdAt,
-        id,
-        title,
-        description,
-        seen,
-        slug,
-        tags,
-        thumbnail {
-          url
-        },
-        mp4 {
-          url
-        }
-      }
-    }
-  `
+  return (
+    <Layout user={user} loading={loading}>
+      <h1>Next.js and Auth0 Example</h1>
 
-    const accountQuery = gql`
-    query {
-    account(where: { id: "ckucogoe0gcy409867pefsfko"}) {
-      username
-      avatar {
-        url
-      }
-    }
-  }
-  `
+      {loading && <p>Loading login info...</p>}
 
-    const data = await graphQLClient.request(videosQuery)
-    const videos = data.videos
-    const accountData = await graphQLClient.request(accountQuery)
-    const account = accountData.account
-
-    return {
-        props: {
-            videos,
-            account
-        }
-    }
-}
-
-
-const Home = ({videos, account}) => {
-
-
-    const randomVideo = (videos) => {
-        return videos[Math.floor(Math.random() * videos.length)]
-    }
-
-    const filterVideos = (videos, genre) => {
-        return videos.filter((video) => video.tags.includes(genre))
-    }
-
-    const unSeenVideos = (videos) => {
-        return videos.filter(video => video.seen == false || video.seen == null)
-    }
-
-    console.log('not seen:', videos.filter(video => video.seen == false || video.seen == null))
-
-
-    return (
+      {!loading && !user && (
         <>
-            <NavBar account={account}/>
-            <div className="app">
-                <div className="main-video">
-                    <img src={randomVideo(videos).thumbnail.url}
-                         alt={randomVideo(videos).title}/>
-                </div>
-
-                <div className="video-feed">
-                    <Link href="#disney">
-                        <div className="franchise" id="disney">
-                            <Image src={disneyLogo}/>
-                        </div>
-                    </Link>
-                    <Link href="#pixar">
-                        <div className="franchise" id="pixar">
-                            <Image src={pixarLogo}/>
-                        </div>
-                    </Link>
-                    <Link href="#star-wars">
-                        <div className="franchise" id="star-wars">
-                            <Image src={starwarsLogo}/>
-                        </div>
-                    </Link>
-                    <Link href="#nat-geo">
-                        <div className="franchise" id="nat-geo">
-                            <Image src={natgeoLogo}/>
-                        </div>
-                    </Link>
-                    <Link href="#marvel">
-                        <div className="franchise" id="marvel">
-                            <Image src={marvelLogo}/>
-                        </div>
-                    </Link>
-                </div>
-                <Section genre={'Recommended for you'} videos={unSeenVideos(videos)}/>
-                <Section genre={'Family'} videos={filterVideos(videos, 'family')}/>
-                <Section genre={'Thriller'} videos={filterVideos(videos, 'thriller')}/>
-                <Section genre={'Classic'} videos={filterVideos(videos, 'classic')}/>
-                <Section id="pixar" genre={'Pixar'} videos={filterVideos(videos, 'pixar')}/>
-                <Section id="marvel" genre={'Marvel'} videos={filterVideos(videos, 'thriller')}/>
-                <Section id="nat-geo" genre={'National Geographic'}
-                         videos={filterVideos(videos, 'national-geographic')}/>
-                <Section id="disney" genre={'Disney'} videos={filterVideos(videos, 'disney')}/>
-                <Section id="star-wars" genre={'Star Wars'} videos={filterVideos(videos, 'star-wars')}/>
-
-
-            </div>
+          <p>
+            To test the login click in <i>Login</i>
+          </p>
+          <p>
+            Once you have logged in you should be able to click in{' '}
+            <i>Profile</i> and <i>Logout</i>
+          </p>
         </>
-    )
+      )}
+
+      {user && (
+        <>
+          <h4>Rendered user info on the client</h4>
+          <img src={user.picture} alt="user picture" />
+          <p>nickname: {user.nickname}</p>
+          <p>name: {user.name}</p>
+        </>
+      )}
+    </Layout>
+  )
 }
 
 export default Home
-
-
-
-
-
-
-
-
-
-
